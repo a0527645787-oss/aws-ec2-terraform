c5f55d59ce9204d3da92b047b7a29485287cbbc8
a0527645787-oss <a0527645787@gmail.com>
2026-06-01 21:21:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_first_server" {
   ami           = "ami-053b0d53c279acc90" # Ubuntu 22.04 LTS באזור us-east-1 המעודכן
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "MyAssignmentInstance"
