5c2efb1b3de004d463b3eea7ddbbb8e4a972111c
a0527645787-oss <a0527645787@gmail.com>
2026-06-01 20:30:19 +0300

    Update EC2 instance AMI to Ubuntu 22.04 LTS

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
-# הגדרת הספק - מול איזה ענן אנחנו עובדים
 provider "aws" {
-  region = "us-east-1" # האזור הגיאוגרפי באמזון (צפון וירג'יניה)
+  region = "us-east-1"
 }
 
-# הגדרת המשאב - יצירת שרת ה-EC2
 resource "aws_instance" "my_first_server" {
-  ami           = "ami-0e2c8ccd4e6379617" # קוד הזיהוי של אובונטו 24.04 באזור זה
-  instance_type = "t2.micro"             # סוג השרת (הגודל שלו)
+  ami           = "ami-053b0d53c279acc90" # Ubuntu 22.04 LTS באזור us-east-1 המעודכן
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "MyAssignmentInstance"         # הבונוס של המשימה: הוספת תגית שם לשרת
+    Name = "MyAssignmentInstance"
   }
 }
